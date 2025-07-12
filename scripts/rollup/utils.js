473bb4a37c2e750841e5549533df063fed96b527
wn <1303682773@qq.com>
2025-07-12 13:52:41 +0900

    feat:reconciler

--- scripts/rollup/utils.js
+++ scripts/rollup/utils.js
@@ -2,6 +2,7 @@ import path from 'node:path';
 import fs from 'node:fs';
 import ts from 'rollup-plugin-typescript2';
 import cjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
 const pkgPath = path.resolve(__dirname, '../../packages');
 const distPath = path.resolve(__dirname, '../../dist/node_modules');
@@ -19,6 +20,9 @@ export function getPkgJSON(pkgName) {
 	const str = fs.readFileSync(path, { encoding: 'utf-8' });
 	return JSON.parse(str);
 }
-export function getBaseRollupPlugins({ typescript = {} } = {}) {
-	return [cjs(), ts(typescript)];
+export function getBaseRollupPlugins({
+	alias = { __DEV__: true },
+	typescript = {}
+} = {}) {
+	return [replace(alias), cjs(), ts(typescript)];
 }
